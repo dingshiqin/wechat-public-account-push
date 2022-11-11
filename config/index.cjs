a17425549529f657a62fb44001332340fe83bcf6
dingshiqin <117984523+dingshiqin@users.noreply.github.com>
2022-11-11 19:08:45 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,25 +5,25 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {
+const USER_CONFIG = {oLtHa5w9kM91i4qbn2J2tbWQYsUE
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx5a88b428319a332dE',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'd47cf2f5a88f792e621ec311ad92a979',
 
   PROVINCE: '成都',
   CITY: '成都',
 
-  USERS: [
+  USERS: [oLtHa5w9kM91i4qbn2J2tbWQYsUE
     {
       // 想要发送的人的名字
       name: '蒲先生',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oLtHa5w9kM91i4qbn2J2tbWQYsUE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'CIKTflRzer_vgs_gl2jIF-pGxAPUNuwkC1v3UzCmS-0 ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
